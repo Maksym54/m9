529bfa6320c4d099355bb4ed51b86e059e2362fe
Maksym54 <scvrty33@gmail.com>
2024-01-05 12:20:29 +0200

    up.py

--- up.py
+++ up.py
@@ -5,7 +5,7 @@ from save import quotes_json
 client = boto3.client('lambda', region_name='eu-central-1')
 
 response = client.invoke(
-  FunctionName='my-function-name',
+  FunctionName='m8',
   Payload=quotes_json
 )
 
